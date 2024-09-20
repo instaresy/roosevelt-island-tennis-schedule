1c82da02d78057da7dfe2d7f28664a765482265b
dpark068 <dpark068@gmail.com>
2024-09-19 23:12:25 -0400

    (feat) add 3 more accounts & change sunday times

--- config/accounts.py
+++ config/accounts.py
@@ -1,29 +1,43 @@
-from util.constants import USERNAME_1, PW_1, USERNAME_2, PW_2, USERNAME_3, PW_3, USERNAME_4, PW_4, USERNAME_5, PW_5
+from util.constants import USERNAME_1, PW_1, \
+                            USERNAME_2, PW_2, \
+                            USERNAME_3, PW_3, \
+                            USERNAME_4, PW_4, \
+                            USERNAME_5, PW_5, \
+                            USERNAME_6, PW_6, \
+                            USERNAME_7, PW_7, \
+                            USERNAME_8, PW_8
 
 accounts_config = {
     "account1": {
         "username": USERNAME_1,
-        "password": PW_1,
-        "court": 1
+        "password": PW_1
     },
     "account2": {
         "username": USERNAME_2,
-        "password": PW_2,
-        "court": 1
+        "password": PW_2
     },
     "account3": {
         "username": USERNAME_3,
-        "password": PW_3,
-        "court": 1
+        "password": PW_3
     },
     "account4": {
         "username": USERNAME_4,
-        "password": PW_4,
-        "court": 1
+        "password": PW_4
     },
     "account5": {
         "username": USERNAME_5,
-        "password": PW_5,
-        "court": 1
+        "password": PW_5
+    },
+    "account6": {
+        "username": USERNAME_6,
+        "password": PW_6
+    },
+    "account7": {
+        "username": USERNAME_7,
+        "password": PW_7
+    },
+    "account8": {
+        "username": USERNAME_8,
+        "password": PW_8
     }
 }
